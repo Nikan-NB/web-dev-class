73a89cf5fbcf02fca6e93a312159ff68a9a9702b
Nikan <nikan.nb1@gmail.com>
2021-08-15 23:30:30 +0430

    week6 s1 project

---
+++ week6/s1/class/js/app.js
@@ -0,0 +1,24 @@
+// console.log("test ok");
+
+var x = 10;//number
+var y = 10.5;//number
+var z ='nikan';//string
+var a = false;//boolean
+var b = true;//boolean
+
+console.log(x,y,z,a,b);
+
+var myName = 'nikan';
+var myAge = 14;
+console.log(myName,myAge);
+
+var firstName = 'Nikan';
+var lastName = 'Nasiri';
+
+console.log('I am ' +firstName + ' ' + lastName +' '+ 'I am' + ' ' + myAge)
+
+var firstNumber = 10;
+var secondNumber = 12;
+var sum =firstNumber+secondNumber
+
+console.log('sum = ' +  sum)
---
+++ week6/s2/class/app.js
@@ -0,0 +1,10 @@
+// var x = 10;
+// var y = 10;
+// var z = 10;
+// var w = 12;
+
+// if(x == y && x == z && z == w){
+//     console.log('equal');
+// }else {
+//     console.log('not equal')
+// }
